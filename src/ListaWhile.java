e62619647cdab870d8616ebc46fa9c616e85ad3a
GustavoPrim <gugahprm@gmail.com>
2023-03-01 17:28:36 -0300

    Feito o primeiro exercício da lista de While

--- src/ListaWhile.java
+++ src/ListaWhile.java
@@ -4,6 +4,18 @@ public class ListaWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        int hora = 23;
+        while (hora >= 0){
+            int minutos = 59;
+            while (minutos >= 0){
+                int segundos = 59;
+                while(segundos >= 0){
+                    System.out.println(hora + ":" + minutos + ":" + segundos);
+                    segundos -= 1;
+                }
+                minutos -= 1;
+            }
+            hora -= 1;
+        }
     }
 }
